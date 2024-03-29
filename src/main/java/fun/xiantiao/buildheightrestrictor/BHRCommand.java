bf5e1556fab70e5ba7eb9c4f50a54f1601b3ee6d
xiantiao <3054587546@qq.com>
2024-03-29 21:44:18 +0800

    构建成功

---
+++ src/main/java/fun/xiantiao/buildheightrestrictor/BHRCommand.java
@@ -0,0 +1,45 @@
+package fun.xiantiao.buildheightrestrictor;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static fun.xiantiao.buildheightrestrictor.BuildHeightRestrictor.getInstance;
+
+/**
+ * @author xiantiao
+ * @date 2024/3/29
+ * BuildHeightRestrictor
+ */
+public class BHRCommand implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if (!("bhr".equalsIgnoreCase(command.getName()) && "buildheightrestrictor".equalsIgnoreCase(command.getName()))) {
+            return false;
+        }
+
+        if (args.length == 1 && sender.hasPermission("buildheightrestrictor.bypass") || (sender.isOp() && getInstance().getConfig().getBoolean("op"))) {
+            if ("reload".equalsIgnoreCase(args[0])) {
+                getInstance().reloadConfig();
+                sender.sendMessage("reload ok!");
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String s, String[] args) {
+        List<String> list = new ArrayList<>();
+
+        if (args.length == 1 && sender.hasPermission("buildheightrestrictor.bypass") || (sender.isOp() && getInstance().getConfig().getBoolean("op"))) {
+            list.add("reload");
+        }
+
+        return list;
+    }
+}
---
+++ src/main/java/fun/xiantiao/buildheightrestrictor/BuildHeightRestrictor.java
@@ -0,0 +1,58 @@
+package fun.xiantiao.buildheightrestrictor;
+
+import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author xiantiao
+ */
+public final class BuildHeightRestrictor extends JavaPlugin implements Listener {
+
+    private static BuildHeightRestrictor instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig();
+        reloadConfig();
+
+        getServer().getPluginManager().registerEvents(this,this);
+
+        PluginCommand buildheightrestrictor = getCommand("buildheightrestrictor");
+        if (buildheightrestrictor != null) {
+            buildheightrestrictor.setExecutor(new BHRCommand());
+            buildheightrestrictor.setTabCompleter(new BHRCommand());
+        } else {
+            getLogger().warning("加载指令时出现错误，指令将无法使用");
+            getLogger().warning("An error occurred while loading instructions, and the instructions will be unusable");
+        }
+
+        new Metrics(this, 21462);
+    }
+
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+
+        if (player.isOp() && getConfig().getBoolean("op")) {return;}
+        if (player.hasPermission("buildheightrestrictor.bypass")) {return;}
+
+        if (event.getBlockPlaced().getY() > getConfig().getInt("Height")) {
+            event.setBuild(false);
+            for (String message : getConfig().getStringList("Message")) {
+                player.sendMessage(message);
+            }
+        }
+    }
+
+    public static BuildHeightRestrictor getInstance() {
+        return instance;
+    }
+}
